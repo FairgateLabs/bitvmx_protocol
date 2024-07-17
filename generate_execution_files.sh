d54eb4618dedca3c5ebe417c98ad3950f6adc6b0
ramonamela <ramon.amela@gmail.com>
2024-07-17 20:13:13 +0200

    Update instructions and commit generated trace coherent with bitvmx-cpu-internal version

--- generate_execution_files.sh
+++ generate_execution_files.sh
@@ -2,3 +2,6 @@
 cargo run --manifest-path ./BitVMX-CPU-Internal/Cargo.toml -p emulator -- instruction-mapping > ./execution_files/instruction_mapping.txt
 # Generate instruction commitment
 cargo run --manifest-path ./BitVMX-CPU-Internal/Cargo.toml -p emulator -- generate-rom-commitment --elf ./BitVMX-CPU-Internal/docker-riscv32/plainc.elf > ./execution_files/instruction_commitment.txt
+
+cargo run --manifest-path ./BitVMX-CPU-Internal/Cargo.toml -p emulator -- execute --elf docker-riscv32/plainc.elf --input 01 -t > ./verifier_files/execution_trace.txt
+cargo run --manifest-path ./BitVMX-CPU-Internal/Cargo.toml -p emulator -- execute --elf docker-riscv32/plainc.elf --input 01 -t > ./prober_files/execution_trace.txt
